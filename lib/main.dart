0eb857b1e8d8feda4ee604449a2c9f9c3f41cad3
SrikanthErra <115687169+SrikanthErra@users.noreply.github.com>
2022-10-17 14:46:12 +0530

    Axis Alignment commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text("Flutter first app"))),
+        body: Center(
+            child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Colors.red),
+                  color: Colors.grey,
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      color: Colors.orange,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.white,
+                      width: 50,
+                      height: 50,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 50,
+                      height: 50,
+                    )
+                  ],
+                )))),
+  ));
+}
